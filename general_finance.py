a467576e8d93d83c548993f4e2795e9fbd32e76a
Bridget Scanlan <bridget.l.scanlan@gmail.com>
2020-02-01 18:20:05 +1000

    Added account class with init and change methods

--- general_finance.py
+++ general_finance.py
@@ -1,10 +1,41 @@
 #!/usr/bin/env python3
+import datetime
 
 class Account(object):
-    ''' '''
-    def __init__(number, name, find_str):
-        ''' '''
-        pass
+    '''A class for bank accounts.'''
+    def __init__(self, number, name, find_str, current_bal=0):
+        ''' A bank account instance that is findable in data.
+        
+            Number is the account number, which must be either an intiger 
+            or a string. Name is the reference name of the account (e.g. 
+            'spending'), as a string. Find_str is the unique string that 
+            appears in the data file identifying this account. Current_bal 
+            is the current balance in dollars, set to zero by default.
+            
+            Constructor: Account(int/str, str, str, float) --> Account
+        '''
+
+        self.num = str(number)
+        self.name = name
+        self.finder = find_str
+        self.balance = current_bal
+        self.balance_hist = []
+        self.time_hist = []
+
+    def change_balance(self, change, date):
+        ''' Modify the balance of the account by the amount given.
+
+            Change is the value of the debit or credit, negative for 
+            debits, given as a float. Date is the date and/or time of 
+            the transaction, given as a datetime instance.
+
+            Account.change_bal(float, datetime) --> None
+        '''
+
+        self.balance += change
+        self.balance_hist.append(self.balance)
+        self.time_hist.append(date)
+
 
 class Transaction(object):
     ''' '''
