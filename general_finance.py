ad5abb5c3f5c00356efda2997807a6eb05824350
ES Alexander <sandman.esalexander@gmail.com>
2020-02-01 18:40:49 +1100

    general finance file for cross-work

---
+++ general_finance.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+class Account(object):
+    ''' '''
+    def __init__(number, name, find_str):
+        ''' '''
+        pass
+
+class Transaction(object):
+    ''' '''
+    def __init__(sender, receiver, date, amount, reason=None, tags=None):
+        ''' '''
+        pass
