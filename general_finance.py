4199f156837d3793bc2ae1265839a47afc0527c2
ES Alexander <sandman.esalexander@gmail.com>
2020-02-01 21:49:37 +1100

    Adjusted file to pass Account tests

--- general_finance.py
+++ general_finance.py
@@ -1,41 +1,44 @@
 #!/usr/bin/env python3
-import datetime
 
 class Account(object):
     '''A class for bank accounts.'''
-    def __init__(self, number, name, find_str, current_bal=0):
+    def __init__(self, number, name, finder, current_bal=0):
         ''' A bank account instance that is findable in data.
-        
-            Number is the account number, which must be either an intiger 
-            or a string. Name is the reference name of the account (e.g. 
-            'spending'), as a string. Find_str is the unique string that 
-            appears in the data file identifying this account. Current_bal 
+
+            Number is the account number, which must be either an intiger
+            or a string. Name is the reference name of the account (e.g.
+            'spending'), as a string. Find_str is the unique string that
+            appears in the data file identifying this account. Current_bal
             is the current balance in dollars, set to zero by default.
-            
+
             Constructor: Account(int/str, str, str, float) --> Account
         '''
 
-        self.num = str(number)
+        self.number = number
         self.name = name
-        self.finder = find_str
+        self.finder = finder
         self.balance = current_bal
-        self.balance_hist = []
-        self.time_hist = []
+        self.balance_history = []
+        self.time_history = []
 
     def change_balance(self, change, date):
         ''' Modify the balance of the account by the amount given.
 
-            Change is the value of the debit or credit, negative for 
-            debits, given as a float. Date is the date and/or time of 
+            Change is the value of the debit or credit, negative for
+            debits, given as a float. Date is the date and/or time of
             the transaction, given as a datetime instance.
 
             Account.change_bal(float, datetime) --> None
         '''
 
         self.balance += change
-        self.balance_hist.append(self.balance)
-        self.time_hist.append(date)
+        self.balance_history.append(self.balance)
+        self.time_history.append(date)
 
+    def __str__(self):
+        ''' A human-readable representation of this Account. '''
+        print('Account ({!s}):\n\tNumber: {!s}\n\tBalance: {!s}'.format(
+              self.name, self.number, self.balance))
 
 class Transaction(object):
     ''' '''
