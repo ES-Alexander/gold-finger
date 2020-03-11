732ca6661500a5dad277164f17a6996a2e974d2e
ES Alexander <sandman.esalexander@gmail.com>
2020-03-11 15:16:55 +1100

    make plot functions importable without running example code

--- tests/plot.py
+++ tests/plot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import tkinter
+import tkinter as tk
 
 from matplotlib.backends.backend_tkagg import (
     FigureCanvasTkAgg, NavigationToolbar2Tk)
@@ -11,6 +11,7 @@ from matplotlib.figure import Figure
 import numpy as np
 
 def set_plot_labels(ax, title, ylabel=None, xticks=None):
+    '''
     if ax is plt:
         ax.title(title)
         if ylabel:
@@ -18,11 +19,12 @@ def set_plot_labels(ax, title, ylabel=None, xticks=None):
         if xticks:
             ax.xticks(range(len(xticks)), xticks)
     else:
-        ax.set_title(title)
-        if ylabel:
-            ax.set_ylabel(ylabel)
-        if xticks:
-            ax.set_xticklabels(xticks)
+    '''
+    ax.set_title(title)
+    if ylabel:
+        ax.set_ylabel(ylabel)
+    if xticks:
+        ax.set_xticklabels(xticks)
 
 def stacked_column(data, ax, title='title', ylabel='Y', xticks=None,
                    tags=None, relative=False, **kwargs):
@@ -62,64 +64,65 @@ def pie(data, labels, ax, title='title', autopct='%1.1f%%', **kwargs):
     set_plot_labels(ax, title)
 
 
-# generate data
-example_data = np.random.rand(12,3)*100
-times = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-         'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-tags = ['Groceries', 'PTV', 'Climbing']
+if __name__ == '__main__':
+    # generate data
+    example_data = np.random.rand(12,3)*100
+    times = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+             'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    tags = ['Groceries', 'PTV', 'Climbing']
 
-# initialise plot settings
-time_period = '{}-{}'.format(times[0], times[-1])
-abs_title = 'Spending Breakdown ' + time_period
-abs_ylabel = 'Amount Spent ($)'
-rel_title = 'Relative Spending Breakdown ' + time_period
-rel_ylabel = 'Amount Spent (%)'
-cum_title = 'Cumulative Breakdown ' + time_period
-prop_title = 'Spending Proportions ' + time_period
-width = 0.5
+    # initialise plot settings
+    time_period = '{}-{}'.format(times[0], times[-1])
+    abs_title = 'Spending Breakdown ' + time_period
+    abs_ylabel = 'Amount Spent ($)'
+    rel_title = 'Relative Spending Breakdown ' + time_period
+    rel_ylabel = 'Amount Spent (%)'
+    cum_title = 'Cumulative Breakdown ' + time_period
+    prop_title = 'Spending Proportions ' + time_period
+    width = 0.5
 
-# Initialise GUI
-root = tkinter.Tk()
-root.wm_title("Embedding in Tk")
+    # Initialise GUI
+    root = tk.Tk()
+    root.wm_title("Embedding in Tk")
 
-fig = Figure(figsize=(5, 4), dpi=100)
-axs = fig.subplots(2, 2)
-stacked_column(example_data, ax=axs[0,0], ylabel=abs_ylabel, title=abs_title,
-               xticks=times, tags=tags, width=width)
-stacked_column(example_data, ax=axs[0,1], ylabel=rel_ylabel,
-               title=rel_title, xticks=times, tags=tags,
-               relative=True, width=width)
-cum_data = cumulative_line(example_data, ax=axs[1,0], ylabel=abs_ylabel,
-                           title=cum_title, xticks=times, tags=tags)[-1]
-pie(cum_data, ax=axs[1,1], labels=tags, title=prop_title)
-
-
-canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
-canvas.draw()
-canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
-
-toolbar = NavigationToolbar2Tk(canvas, root)
-toolbar.update()
-canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+    fig = Figure(figsize=(5, 4), dpi=100)
+    axs = fig.subplots(2, 2)
+    stacked_column(example_data, ax=axs[0,0], ylabel=abs_ylabel, title=abs_title,
+                   xticks=times, tags=tags, width=width)
+    stacked_column(example_data, ax=axs[0,1], ylabel=rel_ylabel,
+                   title=rel_title, xticks=times, tags=tags,
+                   relative=True, width=width)
+    cum_data = cumulative_line(example_data, ax=axs[1,0], ylabel=abs_ylabel,
+                               title=cum_title, xticks=times, tags=tags)[-1]
+    pie(cum_data, ax=axs[1,1], labels=tags, title=prop_title)
 
 
-def on_key_press(event):
-    print("you pressed {}".format(event.key))
-    key_press_handler(event, canvas, toolbar)
+    canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
+    canvas.draw()
+    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+    toolbar = NavigationToolbar2Tk(canvas, root)
+    toolbar.update()
+    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
 
-canvas.mpl_connect("key_press_event", on_key_press)
+    def on_key_press(event):
+        print("you pressed {}".format(event.key))
+        key_press_handler(event, canvas, toolbar)
 
 
-def _quit():
-    root.quit()     # stops mainloop
-    root.destroy()  # this is necessary on Windows to prevent
-                    # Fatal Python Error: PyEval_RestoreThread: NULL tstate
+    canvas.mpl_connect("key_press_event", on_key_press)
 
 
-button = tkinter.Button(master=root, text="Quit", command=_quit)
-button.pack(side=tkinter.BOTTOM)
+    def _quit():
+        root.quit()     # stops mainloop
+        root.destroy()  # this is necessary on Windows to prevent
+                        # Fatal Python Error: PyEval_RestoreThread: NULL tstate
 
-tkinter.mainloop()
-# If you put root.destroy() here, it will cause an error if the window is
-# closed with the window manager.
+
+    button = tk.Button(master=root, text="Quit", command=_quit)
+    button.pack(side=tk.BOTTOM)
+
+    tk.mainloop()
+    # If you put root.destroy() here, it will cause an error if the window is
+    # closed with the window manager.
